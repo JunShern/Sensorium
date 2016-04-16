f53cf19c84d02f3ae5aa13ad78a0e0763c99c385
JunShern <chanjunshern@gmail.com>
2016-04-16 03:23:59 +0100

    Playing with cv2.threshold()

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def pixelize(img, numCols, numRows) :
             cv2.putText(pixelImg, str(color), (i*rectW+1,(j+1)*rectH-3), font, 0.3, (255,255,255), 1, cv2.LINE_AA)
     return pixelImg
 
-width = 16
-height = 12
+width = 3
+height = 3
 
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('video.mp4')
@@ -36,7 +36,10 @@ while (cap.isOpened()):
     small = cv2.resize(gray, (width, height), interpolation=cv2.INTER_AREA)
 
     pixelImg = pixelize(gray, width, height)
-    cv2.imshow('Pixelized', pixelImg)
+    
+    #ret, threshImg = cv2.threshold(pixelImg,127,255,cv2.THRESH_BINARY)
+    
+    cv2.imshow('Pixelized', threshImg)
 
     # 'q' is for Quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
