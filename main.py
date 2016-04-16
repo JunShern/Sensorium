1787263842a109980a7e72d01ef06a650777f455
JunShern <chanjunshern@gmail.com>
2016-04-16 04:21:51 +0100

    Successful implementation of naive background subtraction with click-calibration

--- main.py
+++ main.py
@@ -16,39 +16,52 @@ def pixelize(img, numCols, numRows) :
 def calibrate(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK: # Double-click
         print "Hello! I was clicked at ", x, y
+        global backgroundImg
+        backgroundImg = currentImg
+
+def naiveSubtract(currentImg, backgroundImg):
+    diff = currentImg - backgroundImg
+    #ret, threshImg = cv2.threshold(pixelImg,127,255,cv2.THRESH_TOZERO)
+    ret, threshImg = cv2.threshold(diff,250,255,cv2.THRESH_TOZERO_INV)
+    return threshImg
 
 # Mouse input
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', calibrate)
+cv2.namedWindow('Difference')
+cv2.setMouseCallback('Difference', calibrate)
 
+# Video setup
 width = 16
 height = 12
-
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('video.mp4')
 w = cap.get(3) # 3 is the video width
 h = cap.get(4) # 4 is the video height
 # Reduce resolution - but there is a minimum. We want the minimum so we set size=1
 # On this laptop webcam, the default is 640x480 and minimum is 160x120
-cap.set(3, 960) 
-cap.set(4, 720)
+w = 640
+h = 480
+cap.set(3, w) #960) 
+cap.set(4, h) #720)
+backgroundImg = np.zeros((h,w),np.uint8)
 
 while (cap.isOpened()):
     # Read frame - if read end of video, ret is false
     ret, frame = cap.read()
     frame = cv2.flip(frame,1) # Flip horizontal makes it mirror-like
-    
-    # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    currentImg = gray.copy()
+    cv2.imshow('Current', currentImg)
+
     # Shrink
     small = cv2.resize(gray, (width, height), interpolation=cv2.INTER_AREA)
-
+    # Pixellate
     pixelImg = pixelize(gray, width, height)
+    #cv2.imshow('Grayscale Pixels', pixelImg)
     
-    #ret, threshImg = cv2.threshold(pixelImg,127,255,cv2.THRESH_BINARY)
+    # Difference
+    diffImg = naiveSubtract(currentImg, backgroundImg)
+    cv2.imshow('Difference', diffImg)
     
-    cv2.imshow('image', pixelImg)
-
     # 'q' is for Quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
